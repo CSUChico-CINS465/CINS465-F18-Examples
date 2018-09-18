8c2b7115dc187e7feb2e2f730a905d22bfcabea5
Bryan Dixon <javawolfpack@gmail.com>
2018-09-18 10:31:36 -0700

    PascalCase Fix?

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(suggestion_model)
+admin.site.register(Suggestion_model)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class suggestion_model(models.Model):
+class Suggestion_model(models.Model):
     suggestion = models.CharField(max_length=240)
 
     def __str__(self):
-        return suggestion
+        return self.suggestion
