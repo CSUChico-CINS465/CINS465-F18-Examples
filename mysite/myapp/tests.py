9ab931e5572143442d5fd11d104af72ebe1b1f60
Bryan Dixon <javawolfpack@gmail.com>
2018-09-20 10:18:49 -0700

    Added a test

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from .models import SuggestionModel
+
 # Create your tests here.
+class SuggestionTestCase(TestCase):
+    def setUp(self):
+        SuggestionModel.objects.create(suggestion="lion")
+        SuggestionModel.objects.create(suggestion="cat")
+
+    def test_suggestion_to_string(self):
+        lion = SuggestionModel.objects.get(suggestion="lion")
+        cat = SuggestionModel.objects.get(suggestion="cat")
+        self.assertEqual(str(lion), 'lion')
+        self.assertEqual(str(cat), 'cat')
