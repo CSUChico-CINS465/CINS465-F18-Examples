b21447717420b987f681426f71591e14beec751a
Bryan Dixon <javawolfpack@gmail.com>
2018-10-10 08:17:22 -0700

    Fix Lint Issues

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -17,11 +17,11 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email",
-            "password1", "password2")
+                  "password1", "password2")
 
     def save(self, commit=True):
-        user=super(RegistrationForm,self).save(commit=False)
-        user.email=self.cleaned_data["email"]
+        user = super(RegistrationForm,self).save(commit=False)
+        user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user
