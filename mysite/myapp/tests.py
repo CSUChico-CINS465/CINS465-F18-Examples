b63bea3066f3e7b7ff98681f8332b63f31dd8876
Bryan Dixon <javawolfpack@gmail.com>
2018-10-18 09:38:20 -0700

    added author in test case

--- mysite/myapp/tests.py
+++ mysite/myapp/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from .models import SuggestionModel
@@ -5,11 +6,14 @@ from .models import SuggestionModel
 # Create your tests here.
 class SuggestionTestCase(TestCase):
     def setUp(self):
-        SuggestionModel.objects.create(suggestion="lion")
-        SuggestionModel.objects.create(suggestion="cat")
+        self.user = User.objects.create_user(
+            username='jacob', email='jacob@…', password='top_secret')
+        SuggestionModel.objects.create(suggestion="lion", author=self.user)
+        SuggestionModel.objects.create(suggestion="cat", author=self.user)
 
     def test_suggestion_to_string(self):
         lion = SuggestionModel.objects.get(suggestion="lion")
         cat = SuggestionModel.objects.get(suggestion="cat")
         self.assertEqual(str(lion), 'lion')
         self.assertEqual(str(cat), 'cat')
+        self.assertEqual(lion.author, self.user)
